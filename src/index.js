c32c527f5acb0a207c28268988f99276b0554410
Alexandre <colasalexandre@gmail.com>
2018-12-06 08:19:01 +0100

    remove validates function (deprecated)

--- src/index.js
+++ src/index.js
@@ -64,23 +64,6 @@ export const validateObject = objectValidators => {
   return validateObjectKey;
 };
 
-/**
- * Validates
- * Use validate with validateKeys
- * @deprecated
- */
-export const validates = curry((validators, value) => {
-  let objectIsValid = true;
-
-  const errors = mapObjIndexed((validate, key) => {
-    const { isValid, errors } = validate(value[key]);
-    !isValid && (objectIsValid = false);
-    return errors;
-  })(validators);
-
-  return { isValid: objectIsValid, errors };
-});
-
 export const conditions = (condition, validator) => {
   let validateCondition = () =>
     is(Array, validator) ? validate(...validator) : validator;
