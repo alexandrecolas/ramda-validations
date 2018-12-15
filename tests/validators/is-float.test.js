1e21f83185815aba5f945ef289cdd6d6d9f238b8
Alexandre <colasalexandre@gmail.com>
2018-12-15 21:11:28 +0100

    Update isFloat

--- tests/validators/is-float.test.js
+++ tests/validators/is-float.test.js
@@ -1,13 +1,27 @@
 import { isFloat } from "src/validators";
 
-test("isFloat return true if value is an integer", () => {
-  expect(isFloat(5.12)).toBe(true);
-});
+describe("isFloat", () => {
+  test("return true if input value is a float number", () => {
+    expect(isFloat(5.12)).toBe(true);
+  });
 
-test("isFloat return true if value is not an integer", () => {
-  expect(isFloat(10.0)).toBe(false);
-  expect(isFloat(10)).toBe(false);
-  expect(isFloat("Hello World")).toBe(false);
-  expect(isFloat(true)).toBe(false);
-  expect(isFloat([])).toBe(false);
+  test("return false if input value is an integer number", () => {
+    expect(isFloat(10.0)).toBe(false);
+  });
+
+  test("return false if input value is an integer number", () => {
+    expect(isFloat(10)).toBe(false);
+  });
+
+  test("return false if input value is a string", () => {
+    expect(isFloat("Hello World")).toBe(false);
+  });
+
+  test("return false if input value is a boolean", () => {
+    expect(isFloat(true)).toBe(false);
+  });
+
+  test("return false if input value is an array", () => {
+    expect(isFloat([])).toBe(false);
+  });
 });
