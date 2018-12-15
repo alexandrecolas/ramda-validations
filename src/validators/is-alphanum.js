3d1de08bd735b1da0c0524014773c9665867a34d
Alexandre <colasalexandre@gmail.com>
2018-12-15 21:02:04 +0100

    Update isAlphanum

--- src/validators/is-alphanum.js
+++ src/validators/is-alphanum.js
@@ -1,7 +1,15 @@
 import { test } from "ramda";
 
 /**
- * isAlphanum
+ * Check that characters of a string are alphanumerics
+ * @param {string} input
+ * @return {boolean}
+ * @exemple
+ *
+ * isAlphanum("123456789") //=> true
+ * isAlphanum("abcd") //=> true
+ * isAlphanum("abcd123456789") //=> true
+ * isAlphanum("abcd1234567,89") //=> false
  */
-const isAlphanum = test(/^[a-zA-Z0-9]*$/);
+const isAlphanum: { (input: string): boolean } = test(/^[a-zA-Z0-9]*$/);
 export default isAlphanum;
