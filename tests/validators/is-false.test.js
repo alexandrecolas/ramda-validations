42bac30415065dc11a88519cea40fad5cce4ab80
Alexandre <colasalexandre@gmail.com>
2018-12-15 21:07:40 +0100

    Update isFalse

--- tests/validators/is-false.test.js
+++ tests/validators/is-false.test.js
@@ -4,8 +4,20 @@ test("isFalse return true if value is false", () => {
   expect(isFalse(false)).toBe(true);
 });
 
+test("isFalse return true if value is false", () => {
+  expect(isFalse(Boolean(false))).toBe(true);
+});
+
 test("isFalse return false if value is true", () => {
   expect(isFalse(true)).toBe(false);
+});
+
+test("isFalse return true if value is false", () => {
+  expect(isFalse(Boolean(true))).toBe(false);
+});
+
+test("isFalse return false if value is true", () => {
+  expect(isFalse(0)).toBe(false);
 });
 
 test("isFalse return false if value is not boolean", () => {
