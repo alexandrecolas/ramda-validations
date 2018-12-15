27dc38eda2fc45f82a4992f3b5745b86aa76d045
Alexandre <colasalexandre@gmail.com>
2018-12-15 21:11:14 +0100

    Update isHash

--- src/validators/is-hash.js
+++ src/validators/is-hash.js
@@ -1,5 +1,13 @@
 /**
- * isHash
+ * Checks if input value is an object hash
+ * @param {any} input
+ * @return {boolean}
+ * @exemple
+ *
+ * isHash({}) //=> true
+ * isHash([]) //=> false
+ * isHash(func) //=> false
+ * isHash(1) //=> false
  */
-const isHash = value => value.constructor == Object;
+const isHash = input => input.constructor == Object;
 export default isHash;
