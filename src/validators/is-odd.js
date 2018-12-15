43e409dd8091d0971981ee45326b587b876698ad
Alexandre <colasalexandre@gmail.com>
2018-12-15 21:12:20 +0100

    Update isOdd

--- src/validators/is-odd.js
+++ src/validators/is-odd.js
@@ -1,5 +1,17 @@
+import { complement, both } from "ramda";
+import isInteger from "./is-integer";
+import isEven from "./is-even";
+
 /**
- * isOdd
+ * Checks if input is odd number.
+ * @param {number} input
+ * @return {boolean}
+ * @exemple
+ *
+ * isOdd(1) //=> true
+ * isOdd(Infinity) //=> false
+ * isOdd(2) //=> false
+ * isOdd(1.1) //=> false
  */
-const isOdd = num => (num % 2 === 1 ? true : false);
+const isOdd = both(isInteger, complement(isEven));
 export default isOdd;
